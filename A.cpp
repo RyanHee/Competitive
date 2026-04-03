adf389c1e1bbf327fcea1765cda5ea7adba451c6
Ryan <heyuanboryan814@gmail.com>
2026-04-02 23:59:14 -0500

    A.cpp

--- A.cpp
+++ A.cpp
@@ -1,4 +1,4 @@
-﻿#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
@@ -10,6 +10,7 @@ using namespace __gnu_pbds;
 #define ar array
 #define pb emplace_back
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define lb(v, x) (int)(lower_bound(all(v), x)-(v).begin())
 #define up(v, x) (int)(upper_bound(all(v), x)-(v).begin())
 #define fi first
@@ -40,19 +41,3 @@ void chmin(T &a, T b) { a=min(a, b); }
 template<class T>
 void chmax(T &a, T b) { a=max(a, b); }
 
-void reset() { 
-    
-}
-
-void solve() {
-
-}
-
-signed main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int t=1;
-    cin >> t;
-    while (t--) 
-        solve();
-    return 0;
-}
